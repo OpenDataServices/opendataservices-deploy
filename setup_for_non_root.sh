b89d363a94963fea8a498c4fef6329e3e3c7302e
Ben Webb <bjwebb67@googlemail.com>
2015-03-27 17:09:55 +0000

    Basic salt-ssh setup

---
+++ setup_for_non_root.sh
@@ -0,0 +1,13 @@
+# Add symlinks to where your SSH public/private keys probably are
+SSH_KEY_DIR=salt-config/pki/ssh
+mkdir -p $SSH_KEY_DIR
+ln -s ~/.ssh/id_rsa $SSH_KEY_DIR/salt-ssh.rsa
+ln -s ~/.ssh/id_rsa.pub $SSH_KEY_DIR/salt-ssh.rsa.pub
+
+# Create a config file with sensible settings for running salt not as root
+mkdir salt-config/master.d
+pwd=`pwd`
+echo "cachedir: $pwd/cache/
+log_file: $pwd/log/
+pki_dir: $pwd/salt-config/pki/
+user: $USER" > salt-config/master.d/localuser.conf
