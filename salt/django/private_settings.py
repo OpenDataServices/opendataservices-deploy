728f8ee82e558651e70aa1217205fc9b2244e872
Ben Webb <bjwebb67@googlemail.com>
2015-05-05 17:12:20 +0100

    Add twitter/googles keys/secrets

--- salt/django/private_settings.py
+++ salt/django/private_settings.py
@@ -7,7 +7,14 @@ SECRET_KEY = '{{ pillar[repo].secret_key }}'
 SENDGRID_PASSWORD = ''
 SENDGRID_USERNAME = ''
 SITE_UNIQUE_ID = ''
+{% if repo == 'opendatacomparison' %}
+SOCIAL_AUTH_TWITTER_KEY = '{{pillar.opendatacomparison.social_auth.twitter.key}}'
+SOCIAL_AUTH_TWITTER_SECRET = '{{pillar.opendatacomparison.social_auth.twitter.secret}}' 
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '{{pillar.opendatacomparison.social_auth.google.key}}'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '{{pillar.opendatacomparison.social_auth.google.secret}}'
+{% else %}
 SOCIAL_AUTH_TWITTER_KEY = ''
 SOCIAL_AUTH_TWITTER_SECRET = ''
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ''
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
+{% endif %}
