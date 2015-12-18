7f9e6b49c1a1bc5f792a086a6a7bc56fa9f58143
Ben Webb <ben.webb@opendataservices.coop>
2015-12-18 13:23:40 +0000

    Add open contracting standard deploy script

---
+++ open-contracting-standard-deploy.sh
@@ -0,0 +1,5 @@
+echo "$PRIVATE_KEY" | tr '#' '\n' | tr '_' ' ' > id_rsa
+chmod 600 id_rsa
+echo '|1|FkTASz83nlFnGSnvrDpt8jGNYko=|iuezK/A43QOIAZied/7LNZ30LGA= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOiwQfg1hM1fcXxtssbgfmrnj2iSNounLWkgeWjU9Fr+slHUpcSt0Gk8o3jihTIXqR3z/KgSPqKmaDv3GIEzwBo=' >> ~/.ssh/known_hosts
+rsync -e 'ssh -i id_rsa' -av --delete build/ tmp-prototype-ocds-docs@dev2.default.opendataservices.uk0.bigv.io:~/web/$TRAVIS_BRANCH
+
