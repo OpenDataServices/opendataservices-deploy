39a19a9492e61f402389d384bd4d67691feb2484
David Spencer <idlemoor@slackbuilds.org>
2019-01-31 13:41:15 +0000

    setup_for_non_root.sh: Tweak commands so this script can be re-run without warnings.

--- setup_for_non_root.sh
+++ setup_for_non_root.sh
@@ -1,11 +1,11 @@
 # Add symlinks to where your SSH public/private keys probably are
 SSH_KEY_DIR=salt-config/pki/ssh
 mkdir -p $SSH_KEY_DIR
-ln -s ~/.ssh/id_rsa $SSH_KEY_DIR/salt-ssh.rsa
-ln -s ~/.ssh/id_rsa.pub $SSH_KEY_DIR/salt-ssh.rsa.pub
+ln -sf ~/.ssh/id_rsa $SSH_KEY_DIR/salt-ssh.rsa
+ln -sf ~/.ssh/id_rsa.pub $SSH_KEY_DIR/salt-ssh.rsa.pub
 
 # Create a config file with sensible settings for running salt not as root
-mkdir salt-config/master.d
+mkdir -p salt-config/master.d
 pwd=`pwd`
 echo "cachedir: $pwd/cache/
 log_file: $pwd/log
