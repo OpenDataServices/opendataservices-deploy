ad15ebe29fa18571a559003f8283332341bebbd1
James Baster <james.baster@opendataservices.coop>
2019-06-05 11:12:00 +0100

    Update Open Contracting deploy scripts to point to new server
    This commit is basically the same as https://github.com/open-contracting/standard/commit/8f6bf6a7ebafb0ac5eae1e866aaa03eb71553bf6

--- open-contracting-infrastructure-deploy.sh
+++ open-contracting-infrastructure-deploy.sh
@@ -4,7 +4,7 @@
 echo "$PRIVATE_KEY" | tr '#' '\n' | tr '_' ' ' > id_rsa
 chmod 600 id_rsa
 # Add host key for relevant server
-echo 'dev3.default.opendataservices.uk0.bigv.io ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVUnT1C1bqxOqSwGIRQGbM0KTFDRzG5XeBDosei7I6GCg/9WcsMQ9rjYnOC5YN1j6FoB04S9JU/lzFCnENVTLKvl1mua5Wd209H01QzEV6OL2tC3KbXAMvWtsQSuwPZ0sIYr3PrRBA1eQz6Kd8OaU7+7GdoAUNXJDtgjodmd8yOfE0JQ1dy9XSY6jVUbJ2up+sfexFSO3DR7WlNA3AEi5KBmjqm9R4fI0dnEN/yBKagrAstvg2ojh1KFdjZAZKRRniA2CjkvyhNpiOIWbaPqPNuUhyK3soyCRLZTxcrXafUQ6bdA3wT6RU0QPOxsJJukKHAjBugIH8Fl5DSWODNB53' >> ~/.ssh/known_hosts
+echo 'dev.docs.opencontracting.uk0.bigv.io ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDk8O226B/sYkPqyHdNBdjUFCEpT9IMdUxgFXEOtlPq1QnwTgHY76PsaOin7KhJcBrm8RAOuzoOIrKfgUJjXoxCtx1edp594tD8OChF5koHyO8YkQVlJmH8LrV16txsxokfh2F31ofRIVMk+TXiEfvR4+WehqeR24TwnXzlLIv1KfMJB7znTDdwqZS3uONKjlNNzSBNNIvCZ4WTI6etVlCzQgv4HL9QllKGfk1ctDuwOgsGPMT8f5NNPhI/z7kZkNbcrHJ5Mo6ZtF26qFmZ3Hy6vxJAQ2C4/x/Zemtb0MbIvI4Qlghh3bl5lER1rB54oMg+DidJ36qMrbqEtZxrBwvP' >> ~/.ssh/known_hosts
 # Get lftp binary if necessary
 if hash lftp 2>/dev/null; then
     LFTP=lftp
@@ -16,5 +16,5 @@ fi
 # Make a test ssh connection, as lftp doesn't output key errors so well
 # ssh -i id_rsa ocds-docs@dev3.default.opendataservices.uk0.bigv.io
 # Copy the files to the server
-$LFTP -c "set sftp:connect-program \"ssh -i id_rsa\"; connect sftp://ocds-docs:xxx@dev3.default.opendataservices.uk0.bigv.io; mirror -eR build web/infrastructure/$TRAVIS_BRANCH"
+$LFTP -c "set sftp:connect-program \"ssh -i id_rsa\"; connect sftp://ocds-docs:xxx@dev.docs.opencontracting.uk0.bigv.io; mirror -eR build web/infrastructure/$TRAVIS_BRANCH"
 # Arguments to mirror are -R for recursive and -e to delete old files
