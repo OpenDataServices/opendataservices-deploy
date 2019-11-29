8f3cb38b0f1073f1ee68f0f9c9ccee4a6bc69dba
David Raznick <kindly@gmail.com>
2019-11-29 12:37:54 +0000

    add celeryd config for cleaning processes

--- salt/pwyf-dqt/config.py
+++ salt/pwyf-dqt/config.py
@@ -4,3 +4,5 @@ from default_config import Config as DefaultConfig
 class Config(DefaultConfig):
     SQLALCHEMY_DATABASE_URI = "{{db_uri}}"
     SECRET_KEY = "{{secret_key}}"
+    CELERYD_MAX_TASKS_PER_CHILD = 1
+    CELERYD_PREFETCH_MULTIPLIER = 1
