51925eb23f0bcbceb11204edc632aba480f17bb0
James B <james.baster@opendataservices.coop>
2023-12-01 12:52:40 +0000

    iatitables: no S3

--- salt/iatitables/runner.py
+++ salt/iatitables/runner.py
@@ -5,6 +5,7 @@ os.chdir("{{ dir }}")
 os.environ["IATI_TABLES_SCHEMA"] = "iati"
 os.environ["IATI_TABLES_OUTPUT"] = "{{ dir }}"
 os.environ["DATABASE_URL"] = "{{ db_url }}"
+os.environ["IATI_TABLES_S3_DESTINATION"] = "-"
 
 iatidata.run_all(processes={{ processes }})
 
